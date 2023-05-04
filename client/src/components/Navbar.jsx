bafb6d945215c65e5bfe01d22246318a22b36d91
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-05-04 15:19:31 -0500

    adding logout toast

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
 import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/react"
+import { UnlockIcon } from "@chakra-ui/icons"
 
 export default function Navbar() {
+
+    const toast = useToast()
+
+    const showToast = () => {
+        toast({
+            title: "Logged out",
+            description: "Successfully logged out",
+            duration: 5000,
+            isClosable: true,
+            status: "success",
+            position: "top",
+            icon: <UnlockIcon />
+        })
+    }
+
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center">
       <Heading as="h1" fontSize="1.5em">PLATFORM NAME HERE</Heading>
@@ -10,7 +26,7 @@ export default function Navbar() {
         <Box bg="gray.200" p="10px 15px" borderRadius="50%">G</Box>
         <Text>gordon@email.dev</Text>
         <Button colorScheme="purple">Logout</Button>
-        <Button colorScheme="purple">Checkout</Button>
+        <Button colorScheme="purple" onClick={showToast}>Checkout</Button>
 
       </HStack>
     </Flex>
