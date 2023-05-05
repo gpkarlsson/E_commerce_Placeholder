1979847d5cf075116dd533f2c7582f73dfb2e916
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-04 20:02:43 -0500

    started user controllers, updated user model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,37 @@ const User = require("../models/User");
 module.exports = {
     //add user controller logic
 
-    //GET a user by email/pass
+  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+  async createUser({ body }, res) {
+    const user = await User.create(body);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+
+  async login({ body }, res) {
+    const user = await User.findOne([{ password: body.password }, { email: body.email }] );
+    if (!user) {
+      return res.status(400).json({ message: "Can't find this user" });
+    }
+
+    const correctEmail = await user.isCorrectEmail(body.password);
+
+    if (!correctEmail) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
 
     //GET all items in cart
 
