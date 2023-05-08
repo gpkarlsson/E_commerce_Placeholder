80aa10bb6559283698f0c1f1a02353c53942de68
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-05-08 12:44:49 -0500

    working on login page and navbar responsiveness

---
+++ client/src/layouts/LoginLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Footer from '../components/Footer'
+
+export default function LoginLayout({ children }) {
+  return (
+    <Box bg="gray.100" minH="100vh">
+      {/* your custom header here */}
+      <Box maxW="md" mx="auto" py={4}>
+        {children}
+      </Box>
+      <Footer />
+    </Box>
+  )
+} 
