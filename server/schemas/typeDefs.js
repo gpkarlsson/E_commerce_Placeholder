fe0cbb949388fcffe5b98d20bfc9be67537c05e7
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-05-10 13:30:10 -0500

    modifed typeDefs more, got apollo graphql running on port 3001

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,3 @@
-// @ts-check
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -18,13 +17,14 @@ const typeDefs = gql`
         itemDescription: String
     }
     type History {
-    
         _id: ID!
         user_id: Int!
-        order_date: Int!
+        order_date: String!
         order: [Item]
     }
-    type CartInput {
+    #History - user_id: Number! -> user_id: Int!
+    #History - order_date: Date! may be causing errors as well?
+    input CartInput {
         itemId: String
         user_id: String
         itemName: String
@@ -32,6 +32,7 @@ const typeDefs = gql`
         price: String
         itemDescription: String
     }
+    #type CartInput -> input CartInput
     type Auth {
         token: ID!
         user: User
@@ -46,19 +47,31 @@ const typeDefs = gql`
         currentUser: User
         currentUserItems: UserItems
         currentUserHistory: UserHistory
-        currentUserCart: [Item]
+        currentUserCart: User
     }
+    #Query - currentUserCart not defined in schema
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(email: String!, password: String!, username: String!): Auth
         putItemInCart(Item: CartInput!): User
         removeItemInCart(Item: CartInput!): User
         emptyCart(User: ID): User
-        addItem(user_id: String!, itemName: String!, imageLink: String!, price: String!, itemDescription: String!): Item
-        #removeItem(itemId: ID!): Item
-        #checkout(user_id: String!, order_date: String!, order: [Item]): History 
+        addItem(user_id: String!, itemName: String!, imageLink: String!, price: String!, itemDescription: String!): Item 
+        removeItem(itemId: ID!): Item
+        checkout(order: [ItemInput]!): History
     }
-    
+    #The type of Mutation.checkout(order:) must be Input Type but got: [Item]!.
+    input ItemInput {
+        itemId: String
+        user_id: String
+        itemName: String
+        imageLink: String
+        price: String
+        itemDescription: String 
+    }
+
+    #Mutation - checkout not defined in schema
+    #Mutation - removeItem not defined in schema
 `;
 
 module.exports = typeDefs;
