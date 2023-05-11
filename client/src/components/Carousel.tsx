04f80217fd7f861712af0538a95bcb0a54d93dc5
gordonk <gpkarlsson72@gmail.com>
2023-05-10 19:41:05 -0500

    fixing typo

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -21,7 +21,7 @@ const settings = {
 export default function Carousel() {
   // As we have used custom buttons, we need a reference variable to
   // change the state
-  const [slider, setSlider] = React.useState<typeof Slider | null>(null);
+  const [slider, setSlider] = React.useState< Slider | null>(null);
 
   // These are the breakpoints which changes the position of the
   // buttons as the screen size changes
