eca3e34586db689c2e5ce86b7b27ad5487a78137
Ben Ropa <benropa@outlook.com>
2023-05-14 18:03:44 -0500

    updates to about us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function AboutUs() {
   return (
     <div>AboutUs</div>,
+    <div>Devin Hoffmaster:  </div>,
     <div>Gordon Karlsson:  </div>,
     <div>Stephen Merki:  </div>,
-    <div>Devin Hoffmaster:  </div>,
     <div>Ben Ropa:  </div>
   )
 }
