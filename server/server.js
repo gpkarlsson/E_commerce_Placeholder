b87f5aec96c65f5194d43340ae02c01c08cb1679
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-15 13:13:54 -0500

    heroku changes

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
