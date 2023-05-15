d4e3ed6e202d6efe630eaee206cbf721b5c6a7d1
stev232 <stev232@yahoo.com>
2023-05-15 17:50:57 -0500

    forgot to add the .. before the slash.

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client')));
+  app.use(express.static(path.join(__dirname, '../client')));
 }
 
 app.get('/', (req, res) => {
